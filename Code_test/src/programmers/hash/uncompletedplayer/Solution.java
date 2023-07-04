7e6c6c83a11eab95b4f620713f6fb4430bf9029d
Kimbumsoo99 <show7441@naver.com>
2023-07-05 06:53:18 +0900

    Java_Coding_Test_Practice 0705

--- Code_test/src/programmers/hash/uncompletedplayer/Solution.java
+++ Code_test/src/programmers/hash/uncompletedplayer/Solution.java
@@ -1,10 +1,29 @@
 package programmers.hash.uncompletedplayer;
+import java.util.HashMap;
 
 public class Solution {
-    public String solution(String[] participant, String[] completion){
-        String answer = "aa";
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        String answer = "";
 
+        for(String name:participant){
+            if(hm.get(name) == null){
+                hm.put(name, 1);
+            }else{
+                hm.put(name, hm.get(name) + 1);
+            }
+            // hm.put(name, hm.getOrDefault(name, 0) + 1);
+        }
+        // System.out.println(hm);
+        for(String name: completion)
+            hm.put(name, hm.get(name) - 1);
 
+        for(String name: hm.keySet()){
+            if(hm.get(name) != 0){
+                answer = name;
+                break;
+            }
+        }
         return answer;
     }
     public static void main(String args[]){
