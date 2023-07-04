23ada86be250ff801db6e9e28e56c3a52b2dbb7a
Kimbumsoo99 <show7441@naver.com>
2023-07-05 06:51:04 +0900

    Java_Coding_Test_Practice 0705

--- Code_test/src/list/linkedlist/implementation/Main.java
+++ Code_test/src/list/linkedlist/implementation/Main.java
@@ -10,13 +10,29 @@ public class Main {
         numbers.addLast(50);
         numbers.addLast(60);
         numbers.add(1, 15);
-        System.out.println(numbers.removeFirst());
-        System.out.println(numbers.remove(0));
-        System.out.println(numbers.removeLast());
-        System.out.println(numbers.size());
-        System.out.println(numbers.get(2));
-        System.out.println(numbers.indexOf(30));
-//        System.out.print(numbers.node(3));
+        LinkedList.ListIterator i = numbers.listIterator();
+//        while(i.hasNext()){
+//            System.out.println(i.next());
+//        }
+        while(i.hasNext()){
+            if((int)i.next() == 20){
+                i.remove();
+            }
+//            System.out.println(i.next());
+        }
+        i.add(5);
+        i.next();
+        i.add(8);
+        i.add(9);
+        i.remove();
         System.out.println(numbers);
+//        System.out.println(numbers.removeFirst());
+//        System.out.println(numbers.remove(0));
+//        System.out.println(numbers.removeLast());
+//        System.out.println(numbers.size());
+//        System.out.println(numbers.get(2));
+//        System.out.println(numbers.indexOf(30));
+////        System.out.print(numbers.node(3));
+//        System.out.println(numbers);
     }
 }
---
+++ Code_test/src/programmers/hash/uncompletedplayer/Solution.java
@@ -0,0 +1,18 @@
+package programmers.hash.uncompletedplayer;
+
+public class Solution {
+    public String solution(String[] participant, String[] completion){
+        String answer = "aa";
+
+
+        return answer;
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+        System.out.println(sol.solution(participant, completion));
+
+    }
+
+}
