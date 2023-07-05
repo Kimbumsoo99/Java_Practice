757c3221ed86e93627806aac11eab083749bac35
Kimbumsoo99 <show7441@naver.com>
2023-07-06 04:26:09 +0900

    Java_Coding_Test_Practice 0706

--- Code_test/src/Main.java
+++ Code_test/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         String str1 = "Hello";
         System.out.println(str1.startsWith("h"));
+        System.out.println(Long.BYTES+ " --> " + Long.SIZE + " --> " + Long.MAX_VALUE); //바이트 크기, 비트 크기, 최대 값(최소값은 MIN)
     }
 }
