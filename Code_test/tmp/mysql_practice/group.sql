f2a5b67a54ced20d0c2e1f5a91da8c53e4fbcbed
Kimbumsoo99 <show7441@naver.com>
2023-07-20 10:45:01 +0900

    Java_Coding_Test_Practice 0720 (SQL practice)

--- Code_test/tmp/mysql_practice/group.sql
+++ Code_test/tmp/mysql_practice/group.sql
@@ -24,3 +24,41 @@ WHERE REGEXP_LIKE(OPTIONS,'통풍시트|열선시트|가죽시트')
 GROUP BY 1 ORDER BY 1
 
 -- WHERE 칼럼명 IN (조건1, 조건2...)는 이 문제에서 사용할 수 없습니다. 하나씩 존재할 수도 있지만, 다른 옵션과도 함께 존재할 수 있기 때문
+
+
+-- level 2, 진료과별 총 예약 횟수 출력하기
+-- 2022년 5월에 예약한 환자 수를 진료과코드 별로 조회, 컬럼명은 '진료과 코드', '5월예약건수'로 지정
+-- 진료과별 예약한 환자 수를 기준으로 오름차순 정렬 -> 같다면 진료과 코드를 기준으로 오름차순
+SELECT MCDP_CD AS 진료과코드, COUNT(APNT_NO) AS 5월예약건수 FROM appointment
+where apnt_ymd like '2022-05%'
+group by MCDP_CD order by 5월예약건수, mcdp_cd;
+
+-- SELECT MCDP_CD AS '진료과 코드', COUNT(APNT_NO) AS '5월예약건수' 오류 코드 ' '로 컬럼명을 묶으면 안됨.
+
+
+-- level 3, 즐겨찾기가 가장 많은 식당 정보 출력하기
+-- 음식종류별로 즐겨찾기수가 가장 많은 식당의 음식 종류, ID, 식당 이름, 즐겨찾기수를 조회하는 SQL문
+-- 음식 종류를 기준으로 내림차순 정렬
+SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES FROM REST_INFO
+WHERE (FOOD_TYPE, FAVORITES) IN
+      (SELECT FOOD_TYPE, MAX(FAVORITES) FROM REST_INFO GROUP BY FOOD_TYPE)
+ORDER BY 1 DESC;
+
+
+-- level 3, 조건에 맞는 사용자와 총 거래금액 조회하기
+-- 중고 거래의 총금액이 70만 원 이상인 사람의 회원 ID, 닉네임, 총거래금액을 조회하는 SQL문을 작성
+-- 총거래금액을 기준으로 오름차순 정렬
+SELECT u.user_id, u.nickname, sum(b.price) as TOTAL_SALES
+from used_goods_board b join used_goods_user u on b.writer_id = u.user_id
+where b.status ='DONE'
+group by 1 having TOTAL_SALES >= 700000 order by 3;
+
+
+-- level 3, 카테고리 별 도서 판매량 집계하기
+-- 2022년 1월의 카테고리 별 도서 판매량을 합산하고, 카테고리(CATEGORY), 총 판매량(TOTAL_SALES) 리스트를 출력
+-- 카테고리명을 기준으로 오름차순 정렬
+SELECT a.category, sum(b.sales) as total_sales
+from book a join book_sales b on a.book_id=b.book_id
+where b.sales_date like '2022-01%'
+group by category order by 1
+
