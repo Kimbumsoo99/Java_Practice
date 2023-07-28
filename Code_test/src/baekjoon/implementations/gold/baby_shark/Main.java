e763687167f7360a608fc37264f30fbe3012ec4b
Kimbumsoo99 <show7441@naver.com>
2023-07-28 20:58:13 +0900

    Java_Coding_Test_Practice 0728 (아기상어 풀이중)

--- Code_test/src/baekjoon/implementations/gold/baby_shark/Main.java
+++ Code_test/src/baekjoon/implementations/gold/baby_shark/Main.java
@@ -5,6 +5,7 @@ import java.io.*;
 
 public class Main {
     static int[][] Graph;
+    static boolean[][] visit;
     static int[] dx = {0, -1, 1, 0};
     static int[] dy = {-1, 0, 0, 1};
 
@@ -16,6 +17,7 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         Graph = new int[N][N];
+        visit = new boolean[N][N];
         int[] start = new int[2];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -37,28 +39,37 @@ public class Main {
         int hungry = 0;
         ArrayDeque<int[]> dq = new ArrayDeque<>();
         dq.offerLast(new int[]{start[0], start[1], 0, start[0], start[1]}); // dq Y, dq X, time), 상어 Y, 상어 X
+        visit[start[0]][start[1]] = true;
 
         while (!dq.isEmpty()) {
             int[] tmp = dq.pollFirst();
+//            System.out.println(tmp[0] + " " + tmp[1]);
 
             for (int i = 0; i < 4; i++) {
                 int nextX = tmp[1] + dx[i];
                 int nextY = tmp[0] + dy[i];
-
                 if (nextY >= 0 && nextX >= 0 && nextY < Graph.length && nextX < Graph.length) {
-                    if (Graph[nextY][nextX] <= size) {
+                    if (!visit[nextY][nextX] && Graph[nextY][nextX] <= size) {
                         if(Graph[nextY][nextX] != size && Graph[nextY][nextX] != 0){
                             Graph[tmp[3]][tmp[4]] = 0;
                             Graph[nextY][nextX] = 9;
-                            time += tmp[2];
+                            time += tmp[2] + 1;
                             hungry++;
                             if (hungry == size) {
                                 size++;
                                 hungry = 0;
                             }
+                            System.out.println(nextY + " " + nextX + " " + time);
+                            visit = new boolean[Graph.length][Graph.length];
                             dq.clear();
                             dq.offerLast(new int[]{nextY, nextX, 0, nextY, nextX});
+                            visit[nextY][nextX] = true;
+//                            System.out.println(dq.peek()[0] + " d " + dq.peek()[1]);
                         }else{
+                            if (time != 0) {
+                                System.out.println(tmp[0] + " " + tmp[1] + "   " + nextY + " " + nextX + " " + tmp[2]);
+                            }
+                            visit[nextY][nextX] = true;
                             dq.offerLast(new int[]{nextY, nextX, tmp[2] + 1, tmp[3], tmp[4]});
                         }
                     }
