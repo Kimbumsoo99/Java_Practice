efe970c4a50ddc2416763dc75e54b090defed7ab
Kimbumsoo99 <show7441@naver.com>
2023-07-28 23:51:26 +0900

    Java_Coding_Test_Practice 0728 (아기상어 풀이중)

--- Code_test/src/baekjoon/implementations/gold/baby_shark/Main.java
+++ Code_test/src/baekjoon/implementations/gold/baby_shark/Main.java
@@ -11,6 +11,16 @@ public class Main {
 
     static int size = 2;
 
+    static void drowGraph(){
+        for (int i = 0; i < Graph.length; i++) {
+            for (int j = 0; j < Graph.length; j++) {
+                System.out.print(Graph[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -23,7 +33,7 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 int tmp = Integer.parseInt(st.nextToken());
-                Graph[i][j] = tmp;
+                Graph[i][j] = tmp != 9 ? tmp : -1;
                 if (tmp == 9) {
                     start = new int[]{i, j};
                 }
@@ -43,7 +53,7 @@ public class Main {
 
         while (!dq.isEmpty()) {
             int[] tmp = dq.pollFirst();
-//            System.out.println(tmp[0] + " " + tmp[1]);
+//            System.out.println(tmp[0] + " " + tmp[1] + " " + tmp[2] + " " + Graph[tmp[0]][tmp[1]]);
 
             for (int i = 0; i < 4; i++) {
                 int nextX = tmp[1] + dx[i];
@@ -51,24 +61,46 @@ public class Main {
                 if (nextY >= 0 && nextX >= 0 && nextY < Graph.length && nextX < Graph.length) {
                     if (!visit[nextY][nextX] && Graph[nextY][nextX] <= size) {
                         if(Graph[nextY][nextX] != size && Graph[nextY][nextX] != 0){
+//                            if (tmp[1] - 1 >= 0 && i == 0 && Graph[tmp[0]][tmp[1] - 1] < size && Graph[tmp[0]][tmp[1] - 1] != 0) {
+//                                System.out.println(tmp[0] + " " + (tmp[1] - 1) + " " + time);
+//                                continue;
+////                                nextX = tmp[1] - 1;
+////                                nextY = tmp[0];
+//                            }
+                            if (i == 3 && dq.peekFirst() != null
+                                && dq.peekFirst()[1] + 1 < Graph.length
+                                && Graph[tmp[0]][dq.peekFirst()[1] + 1] < size
+                                && Graph[tmp[0]][dq.peekFirst()[1] + 1] != 0 && tmp[2] == dq.peekFirst()[2] && tmp[0] <= dq.peekFirst()[0]) {
+                                System.out.println("ddddddd " + tmp[0] + " " + (dq.peekFirst()[1] + 1) + " " + Graph[tmp[0]][dq.peekFirst()[1] + 1]);
+                                continue;
+                            }
+//                            if (dq.peekFirst() != null && dq.peekFirst()[0] < tmp[0] && dq.peekFirst()[2] <= tmp[2]) {
+//                                continue;
+//                            }
+                            if (isEat(dq, tmp[2], tmp[0])) {
+//                                System.out.println(tmp[0] + " " + tmp[1]);
+                                continue;
+                            }
                             Graph[tmp[3]][tmp[4]] = 0;
-                            Graph[nextY][nextX] = 9;
+                            Graph[nextY][nextX] = -1;
                             time += tmp[2] + 1;
                             hungry++;
                             if (hungry == size) {
                                 size++;
                                 hungry = 0;
                             }
-                            System.out.println(nextY + " " + nextX + " " + time);
+                            System.out.println(tmp[0] + " " + tmp[1] + " " + nextY + " " + nextX + " " + time);
+//                            drowGraph();
                             visit = new boolean[Graph.length][Graph.length];
                             dq.clear();
                             dq.offerLast(new int[]{nextY, nextX, 0, nextY, nextX});
                             visit[nextY][nextX] = true;
+                            break;
 //                            System.out.println(dq.peek()[0] + " d " + dq.peek()[1]);
                         }else{
-                            if (time != 0) {
-                                System.out.println(tmp[0] + " " + tmp[1] + "   " + nextY + " " + nextX + " " + tmp[2]);
-                            }
+//                            if (time != 0) {
+//                                System.out.println(tmp[0] + " " + tmp[1] + "   " + nextY + " " + nextX + " " + tmp[2]);
+//                            }
                             visit[nextY][nextX] = true;
                             dq.offerLast(new int[]{nextY, nextX, tmp[2] + 1, tmp[3], tmp[4]});
                         }
@@ -79,4 +111,17 @@ public class Main {
 
         return time;
     }
+
+    static boolean isEat(ArrayDeque<int[]> tmpQ, int time, int y) {
+        for (int[] tmp : tmpQ) {
+//            System.out.println(tmp[0] + " " + tmp[1] +  " " + tmp[2] + " y는? " + y + " time은? " + time);
+            if (tmp[2] <= time) {
+                if (tmp[0] < y) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
