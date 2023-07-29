6a8e79b85099e7fa48e01cf9dccec2a32089c1bd
Kimbumsoo99 <show7441@naver.com>
2023-07-29 14:03:36 +0900

    Java_Coding_Test_Practice 0729

--- Code_test/src/baekjoon/implementations/gold/people_move/Main.java
+++ Code_test/src/baekjoon/implementations/gold/people_move/Main.java
@@ -33,26 +33,56 @@ public class Main {
             }
         }
 
-        ArrayList<ArrayList<int[]>> unionList = new ArrayList<>();
-        while (unionList.isEmpty()) {
+        int day = 0;
+        while (true) {
+            int count = 0;
+            day++;
             visit = new boolean[N][N];
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     if (!visit[i][j]) {
-                        unionList.add(bfs(i, j));
+                        bfs(i, j);
+                        count++;
+//                        drawVisit();
                     }
                 }
             }
+            if (count == (int) Math.pow(N, 2)) {
+                break;
+            }
         }
+        System.out.println(day - 1);
+//        drawGraph();
     }
 
-    static ArrayList<int[]> bfs(int y, int x){
+    static void drawGraph(){
+        for (int i = 0; i < Graph.length; i++) {
+            for (int j = 0; j < Graph[0].length; j++) {
+                System.out.print(Graph[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    static void drawVisit(){
+        for (int i = 0; i < visit.length; i++) {
+            for (int j = 0; j < visit[0].length; j++) {
+                System.out.print(visit[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    static void bfs(int y, int x){
         ArrayDeque<int[]> dq = new ArrayDeque<>();
         ArrayList<int[]> union = new ArrayList<>();
-        dq.offerLast(new int[]{y, x, Graph[y][x]});
-        union.add(new int[]{y, x, Graph[y][x]});
-        visit[y][x] = true;
         int sum = Graph[y][x];
+
+        visit[y][x] = true;
+        dq.offerLast(new int[]{y, x, Graph[y][x]});
+        union.add(new int[]{y, x});
+
         while (!dq.isEmpty()) {
             int[] tmp = dq.pollFirst();
             for (int i = 0; i < 4; i++) {
@@ -61,25 +91,23 @@ public class Main {
 
                 if (nextY >= 0 && nextX >= 0 && nextX < Graph.length && nextY < Graph.length) {
                     int p = Math.abs(Graph[tmp[0]][tmp[1]] - Graph[nextY][nextX]);
-                    if (p >= L && p <= R) {
+                    if (!visit[nextY][nextX] && p >= L && p <= R) {
                         // 1. 국경선안에 존재한다면, dq에 넣는다.
                         dq.offerLast(new int[]{nextY, nextX, Graph[y][x]});
                         // 2. visit[nY][nX] = true하면서 국경선끼리의 연합을 만든다.
                         visit[nextY][nextX] = true;
                         // 3. 이때 각각의 dq 연합에 대하여, 합을 만든다.
                         sum += Graph[nextY][nextX];
-                        union.add(new int[]{nextY, nextX, sum});
+                        union.add(new int[]{nextY, nextX});
                     }
                 }
             }
         }
-        return union;
-        // 연합의 합을 연합 국가 수만큼 나눈 뒤, 각각의 그래프 좌표에 저장한다.
-//        sum = (int) sum / unionList.size();
-//        Iterator<int[]> it = unionList.iterator();
-//        while (it.hasNext()) {
-//            int[] tmp = it.next();
-//            Graph[tmp[0]][tmp[1]] = sum;
-//        }
+        sum /= union.size();
+        Iterator<int[]> it = union.iterator();
+        while (it.hasNext()) {
+            int tmp[] = it.next();
+            Graph[tmp[0]][tmp[1]] = sum;
+        }
     }
 }
