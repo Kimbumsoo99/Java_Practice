e1017d8b8bbcd0ff9d1def09d2e4366a54a67dd7
Kimbumsoo99 <show7441@naver.com>
2023-08-07 02:32:14 +0900

    Java_Coding_Test_Practice 0807

--- Code_test/src/baekjoon/implementations/silver/minecraft/MainBrute.java
+++ Code_test/src/baekjoon/implementations/silver/minecraft/MainBrute.java
@@ -3,13 +3,15 @@ package baekjoon.implementations.silver.minecraft;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+
+// 제출 성공
 public class MainBrute {
     static int[][] map;
     static int N, M, B;
     static int count = 0;
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
---
+++ Code_test/src/programmers/greed/boat/Solution.java
@@ -0,0 +1,22 @@
+package programmers.greed.boat;
+
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int max = people.length - 1;
+        for(int min = 0;min <= max; max--){
+            if(people[min] + people[max] <= limit){
+                min++;
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
