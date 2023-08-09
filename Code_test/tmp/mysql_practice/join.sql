4fdee457152486825312c496d081b2037f615c3c
Kimbumsoo99 <show7441@naver.com>
2023-08-10 04:50:52 +0900

    Java_Coding_Test_Practice 0810 (SQL Practice)

--- Code_test/tmp/mysql_practice/join.sql
+++ Code_test/tmp/mysql_practice/join.sql
@@ -11,3 +11,9 @@ SELECT a.gu, a.dong,
 LEFT OUTER JOIN ant_list b on a.area_cd=b.area_cd AND b.con_year=2020;
 
 
+
+-- programmers
+-- level 2, 조건에 맞는 도서와 저자 리스트 출력하기
+-- '경제' 카테고리에 속하는 도서는 도서 ID가 2, 3인 도서이고, 출판일을 기준으로 오름차순으로 정렬
+SELECT A.BOOK_ID, B.AUTHOR_NAME, DATE_FORMAT(A.PUBLISHED_DATE, "%Y-%m-%d") AS PUBLISHED_DATE
+FROM BOOK A JOIN AUTHOR B on a.author_id=b.author_id WHERE CATEGORY = '경제' ORDER BY 3;
