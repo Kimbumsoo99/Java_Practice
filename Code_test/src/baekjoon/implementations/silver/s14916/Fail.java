0e3a8cf2cd797edf43c6be027cf8a305a4e12ab0
Kimbumsoo99 <show7441@naver.com>
2023-09-18 03:06:52 +0900

    Java_Coding_Test_Practice 0918

---
+++ Code_test/src/baekjoon/implementations/silver/s14916/Fail.java
@@ -0,0 +1,27 @@
+package baekjoon.implementations.silver.s14916;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 거스름돈, 실버 V
+public class Fail {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        while (N % 5 != 0) {
+            N -= 2;
+            count++;
+        }
+        System.out.println(N);
+        count += N / 5;
+        if (N < 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(count);
+        }
+    }
+
+}
--- Code_test/src/baekjoon/implementations/silver/s14916/Main.java
+++ Code_test/src/baekjoon/implementations/silver/s14916/Main.java
@@ -4,24 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 거스름돈, 실버 V
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int count = 0;
-        while (N % 5 != 0) {
-            N -= 2;
-            count++;
-        }
-        System.out.println(N);
-        count += N / 5;
-        if (N < 0) {
-            System.out.println(-1);
+        StringBuilder sb = new StringBuilder();
+        if (N == 1 || N == 3) {
+            sb.append(-1);
         } else {
-            System.out.println(count);
+            int count = 0;
+            if (N % 5 == 0) {
+                count = N / 5;
+            } else{
+                count = N / 5;
+                N = N % 5;
+                if (N % 2 == 0) {
+                    count = count + N / 2;
+                } else {
+                    N = N + 5;
+                    count = (count - 1) + N / 2;
+                }
+            }
+            sb.append(count);
         }
+        System.out.println(sb);
     }
 
 }
