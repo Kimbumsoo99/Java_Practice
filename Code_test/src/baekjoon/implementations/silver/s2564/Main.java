b0e4afaaff22503a6d9a1e525256e611f1466173
Kimbumsoo99 <show7441@naver.com>
2023-09-18 15:54:01 +0900

    Java_Coding_Test_Practice 0918 (경비원 풀이중)

---
+++ Code_test/src/baekjoon/implementations/silver/s2564/Main.java
@@ -0,0 +1,70 @@
+package baekjoon.implementations.silver.s2564;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+// 경비원, 실버 I, 2564번
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int X = Integer.parseInt(st.nextToken());
+        int Y = Integer.parseInt(st.nextToken());
+        int shop = Integer.parseInt(br.readLine());
+        ArrayList<int[]> list = new ArrayList<>();
+        for (int i = 0; i < shop; i++) {
+            st = new StringTokenizer(br.readLine());
+            int direction = Integer.parseInt(st.nextToken());
+            int rightDown = Integer.parseInt(st.nextToken());
+            list.add(directionMap(direction, X, Y, rightDown));
+        }
+        st = new StringTokenizer(br.readLine());
+        int startD = Integer.parseInt(st.nextToken());
+        int startRD = Integer.parseInt(st.nextToken());
+        int start[] = directionMap(startD, X, Y, startRD);
+
+        int answer = 0;
+        // 시계, 반시계 해결
+        for (int tmp[] : list) {
+            int shortRange = 0;
+            if (tmp[0] == start[0]) { // 같은 동서남북
+                System.out.println("같은 위치");
+                shortRange = Math.abs(tmp[1] - start[1] + tmp[2] - start[2]);
+            } else if (Math.abs(tmp[0] - start[0]) > 2) { // 건너 편
+                System.out.println("바로 옆");
+                shortRange = Math.abs(tmp[1] - start[1] + tmp[2] - start[2]);
+            } else { // 그 외 -> 바로 옆
+                System.out.println("건너편");
+                shortRange = Math.abs(tmp[1] - start[1] + tmp[2] - start[2]);
+                if (tmp[0] == 1 || tmp[0] == 2) {
+                    shortRange += Math.min(Math.min(tmp[1], start[1]) * 2,
+                        Math.max(tmp[1], start[1]) * 2);
+                } else {
+                    shortRange += Math.min(Math.min(tmp[2], start[2]) * 2,
+                        Math.max(tmp[2], start[2]) * 2);
+                }
+            }
+            System.out.println("shortRange = " + shortRange);
+            answer += shortRange;
+        }
+        System.out.println("answer = " + answer);
+    }
+    static int[] directionMap(int d, int X, int Y, int rightDown) {
+        if (d == 1) {
+            return new int[]{d, rightDown, 0};
+        } else if (d == 2) {
+            return new int[]{d, rightDown, Y};
+        } else if (d == 3) {
+            return new int[]{d, 0, rightDown};
+        }else {
+            return new int[]{d, X, rightDown};
+        }
+    }
+
+
+}
