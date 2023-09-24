4b0dfb055864bf7927d3bd5d39ee4268f0005d25
Kimbumsoo99 <show7441@naver.com>
2023-09-25 04:08:00 +0900

    Java_Coding_Test_Practice 0925

---
+++ Code_test/src/baekjoon/implementations/bronze/b10824/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.implementations.bronze.b10824;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken() + st.nextToken());
+        int M = Integer.parseInt(st.nextToken() + st.nextToken());
+        System.out.println(N + M);
+    }
+
+}
