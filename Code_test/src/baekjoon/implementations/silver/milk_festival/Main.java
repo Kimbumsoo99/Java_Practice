77f9236007a294d984bc5b96c3eadb11cd581268
Kimbumsoo99 <show7441@naver.com>
2023-11-02 23:10:35 +0900

    Java_Coding_Test_Practice 1102

---
+++ Code_test/src/baekjoon/implementations/silver/milk_festival/Main.java
@@ -0,0 +1,29 @@
+package baekjoon.implementations.silver.milk_festival;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int count = 0;
+        int milk = 0;
+        for (int i = 0; i < N; i++) {
+            int tmp = Integer.parseInt(st.nextToken());
+            if (milk == tmp) {
+                count++;
+                milk++;
+                if (milk > 2) {
+                    milk = 0;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+}
