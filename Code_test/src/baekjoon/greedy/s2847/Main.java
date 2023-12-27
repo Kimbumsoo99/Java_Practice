ec0baba5de81114424972554ed9d50950a993117
Kimbumsoo99 <show7441@naver.com>
2023-12-28 03:22:17 +0900

    Java_Coding_Test_Practice 1228

--- Code_test/src/baekjoon/greedy/s2847/Main.java
+++ Code_test/src/baekjoon/greedy/s2847/Main.java
@@ -1,9 +1,6 @@
 package baekjoon.greedy.s2847;
-<<<<<<< HEAD
 
-=======
 // Java_Coding_Test_Practice 1227
->>>>>>> origin/main
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
