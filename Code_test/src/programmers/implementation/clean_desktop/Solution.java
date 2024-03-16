e0ca0cfaa42eadccc3f4e6019e29c31f2a023ebd
김범수 <121588874+Kimbumsoo99@users.noreply.github.com>
2024-03-16 22:56:41 +0900

    Programmers 바탕화면 정리 풀이

---
+++ Code_test/src/programmers/implementation/clean_desktop/Solution.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Solution {
+    int Graph[][], N, M;
+    int maxY, maxX, minY, minX;
+    public int[] solution(String[] wallpaper) {
+        int[] answer = {};
+        N = wallpaper.length;
+        M = wallpaper[0].length();
+        maxY = maxX = Integer.MIN_VALUE;
+        minY = minX = Integer.MAX_VALUE;
+        Graph = new int[N][M];
+        for(int i=0;i<N;i++){
+            char[] arr = wallpaper[i].toCharArray();
+            for(int j=0;j<M;j++){
+                if(arr[j] == '.'){
+                    Graph[i][j] = 0;
+                }else{
+                    Graph[i][j] = 1;
+                    if(minY > i){
+                        minY = i;
+                    }
+                    if(maxY < i+1){
+                        maxY = i+1;
+                    }
+                    if(maxX < j+1){
+                        maxX = j+1;
+                    }
+                    if(minX > j){
+                        minX = j;
+                    }
+                }
+            }
+        }
+        answer = new int[4];
+        answer[0] = minY;
+        answer[1] = minX;
+        answer[2] = maxY;
+        answer[3] = maxX;
+        return answer;
+    }
+}
