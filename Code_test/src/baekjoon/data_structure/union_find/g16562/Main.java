cfd930aa03daba0310ff3e671251bf6cdefe9488
Kimbumsoo99 <show7441@naver.com>
2024-11-23 00:03:40 +0900

    BOJ 16562 친구비 solving

---
+++ Code_test/src/baekjoon/data_structure/union_find/g16562/Main.java
@@ -0,0 +1,81 @@
+package baekjoon.data_structure.union_find.g16562;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] parents, pay;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        // make
+        parents = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            parents[i + 1] = i + 1;
+        }
+
+        pay = new int[N + 1];
+        boolean[] visited = new boolean[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < N + 1; i++) {
+            pay[i] = Integer.parseInt(st.nextToken());
+        }
+
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            union(A, B);
+        }
+
+        int sum = 0;
+
+        for (int i = 1; i <= N; i++) {
+            int idx = find(i);
+
+            if (visited[idx]) {
+                visited[i] = true;
+                continue;
+            }
+
+            sum += pay[idx];
+            visited[idx] = true;
+            visited[i] = true;
+        }
+
+        System.out.println(sum);
+
+        if (sum > K)
+            System.out.println("Oh no");
+        else
+            System.out.println(sum);
+    }
+
+    static int find(int x) {
+        if(parents[x] == x)
+            return x;
+        return parents[x] = find(parents[x]);
+    }
+
+    static void union(int a, int b) {
+        int pA = find(a);
+        int pB = find(b);
+
+        if (pay[pA] >= pay[pB]) {
+            pay[pA] = pay[pB];
+        } else {
+            pay[pB] = pay[pA];
+        }
+    }
+}
