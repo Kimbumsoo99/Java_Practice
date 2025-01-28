c0efb3bca43827d295c688ad4897568ef463dfb1
Kimbumsoo99 <show7441@naver.com>
2025-01-28 23:33:26 +0900

    BOJ 23795 사장님 도박은 재미로 하셔야 합니다 success

---
+++ Code_test/src/baekjoon/math/b23795/Main.java
@@ -0,0 +1,20 @@
+package baekjoon.math.b23795;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int s = 0;
+        while (true) {
+            int N = Integer.parseInt(br.readLine());
+            if (N == -1) {
+                System.out.println(s);
+                return;
+            }
+            s += N;
+        }
+    }
+}
